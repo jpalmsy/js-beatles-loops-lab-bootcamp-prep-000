5c07768e7d436fcd3134619ca61f1068b2180e9c
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-03 00:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function theBeatlesPlay(musicians, instruments) {
   for {let i = 0, l = musicians.length; i < l; i+++] {
     arr.push('${musicians[i]} plays ${instruments[i]} ')
   }
+  return arr
 }
